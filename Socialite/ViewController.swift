b81ef27d00d6d0013666976ee716486c3909953c
Karl Cridland <karlcridland@gmail.com>
2021-05-29 07:05:15 +0100

    Initial Commit

---
+++ Socialite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Socialite
+//
+//  Created by Karl Cridland on 29/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
